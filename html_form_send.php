163653cb36a6b9e6d9afe3414c2e298452418558
Siddharth Mistry <siddmistry97@gmail.com>
2019-09-18 00:08:47 +1200

    Minor text update

--- html_form_send.php
+++ html_form_send.php
@@ -67,7 +67,7 @@ if(isset($_POST['email'])) {
 $headers = 'From: '.$email_from."\r\n".
 'Reply-To: '.$email_from."\r\n" .
 'X-Mailer: PHP/' . phpversion();
-mail($email_to, $email_subject, $email_message, $headers);  
+@mail($email_to, $email_subject, $email_message, $headers);  
 ?>
  
 <!-- place your own success html below -->
